51ca66836931caaafc03e2264e909e1649793c53
Kevin Downey (hiredman) <redchin@gmail.com>
2010-10-03 15:24:27 -0700

    get rid of extra future creation

--- src/conduit/threadpool.clj
+++ src/conduit/threadpool.clj
@@ -4,13 +4,13 @@
 
 (defn- async-pub-reply [fun]
   (fn async-reply [value]
-    (let [fut (future (fun value))]
-      [[(future (first (first @fut)))] async-reply])))
+    (let [fut (future (-> value fun first first))]
+      [[fut] async-reply])))
 
 (defn- async-sg-fn [fun]
   (fn async-reply [value]
-    (let [fut (future (fun value))]
-      (fn [] [[(future (first (first @fut)))] async-reply]))))
+    (let [fut (future (-> value fun first first))]
+      (fn [] [[fut] async-reply]))))
 
 (defn a-async [proc]
   (assoc proc
@@ -19,15 +19,24 @@
     :no-reply (async-pub-reply (:no-reply proc))
     :scatter-gather (async-sg-fn (:scatter-gather proc))))
 
+(defn derefable-future [fut]
+  (reify
+    clojure.lang.IDeref
+    (deref [this] (.get fut))
+    java.util.concurrent.Future
+    (get [this] (.get fut))
+    (get [this timeout units]
+      (.get fut timeout units))))
+
 (defn- threadpool-pub-reply [fun threadpool]
   (fn threadpool-reply [value]
-    (let [fut (.submit threadpool #(fun value))]
-      [[(future (first (first (.get fut))))] threadpool-reply])))
+    (let [fut (.submit threadpool #(-> value fun first first))]
+      [[(derefable-future fut)] threadpool-reply])))
 
 (defn- threadpool-sg-fn [fun threadpool]
   (fn threadpool-reply [value]
-    (let [fut (.submit threadpool #(fun value))]
-      (fn [] [[(future (first (first (.get fut))))] threadpool-reply]))))
+    (let [fut (.submit threadpool #(-> value fun first first))]
+      (fn [] [[(derefable-future fut)] threadpool-reply]))))
 
 (defn a-threadpool [threadpool proc]
   (assoc proc
